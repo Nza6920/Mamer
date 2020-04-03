e83df382493738ae97f89a9096c03193725dd73a
liyingcold <2608953051@qq.com>
2020-04-03 17:46:16 +0800

    修改个人中心界面，话题界面

---
+++ app/src/main/java/com/example/my/mamer/adapter/recycleview/UserBaseAdapter.java
@@ -0,0 +1,83 @@
+package com.example.my.mamer.adapter.recycleview;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public abstract class UserBaseAdapter<T> extends RecyclerView.Adapter<UserBaseAdapter.mViewHolder> {
+
+    private ArrayList<T> listData;
+    private LayoutInflater layoutInflater;
+    private Context context;
+    protected final int mItemLayoutId;
+
+    public UserBaseAdapter(ArrayList<T> listData, Context context, int mItemLayoutId) {
+        this.listData = listData;
+        this.context = context;
+        this.mItemLayoutId = mItemLayoutId;
+        this.layoutInflater=LayoutInflater.from(context);
+    }
+
+    /**
+     * 将布局转换为View
+     * 传递给自定义的ViewHolder实例
+     */
+
+    @Override
+    public mViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view=layoutInflater.inflate(mItemLayoutId,null);
+        mViewHolder viewHolder=new mViewHolder(view);
+        return viewHolder;
+    }
+
+    /**
+     *
+     * @param mViewHolder mViewHolder
+     * @param i 位置
+     *     对recycleView子项进行赋值，每个子项滚动到屏幕内时执行
+     */
+
+    @Override
+    public void onBindViewHolder(UserBaseAdapter.mViewHolder mViewHolder, final int i) {
+        if (onItemClickListener!=null){
+            mViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(i);
+                }
+            });
+        }
+    }
+
+//    获取item的数量
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+//    view为recycler子项最外层布局（user_topics_item.xml）
+    public class mViewHolder extends RecyclerView.ViewHolder{
+        public mViewHolder(View view){
+            super(view);
+        }
+        public <V extends View>V view(int id){
+            return itemView.findViewById(id);
+        }
+    }
+
+//    点击事件
+    public interface OnItemClickListener{
+        public void onItemClick(int position);
+    }
+
+    OnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+}
